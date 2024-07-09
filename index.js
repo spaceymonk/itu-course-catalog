190b45e630aab8c04acf508e8fc40d13eb426c7f
spaceymonk <b.kaan.ozkan@gmail.com>
2024-07-09 13:45:55 +0300

    Get course codes in a list

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 "use strict";
-console.log("Hello world!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+const node_https_1 = __importDefault(require("node:https"));
+const node_fs_1 = __importDefault(require("node:fs"));
+const jsdom_1 = require("jsdom");
+const courseListUrl = "https://www.sis.itu.edu.tr/EN/student/undergraduate/course-plans/plans/BLGE/201810.html";
+const courseListFilename = "CourseList.html";
+function GetCourseCodes(document) {
+    const elements = document.querySelectorAll('a[title="Click For Course Catalogue Form."]');
+    const result = [];
+    elements.forEach((element) => {
+        const text = element.innerHTML;
+        const regex = /\s+/;
+        const splits = text.split(regex);
+        result.push({ code: splits[0], number: splits[1] });
+    });
+    return result;
+}
+function GetCourseList(data) {
+    const jsdom = new jsdom_1.JSDOM(data, { url: courseListUrl });
+    const courseCodes = GetCourseCodes(jsdom.window.document);
+    courseCodes.forEach((course) => {
+        console.log(JSON.stringify(course));
+    });
+}
+if (node_fs_1.default.existsSync(courseListFilename)) {
+    console.log("Reading from cache: " + courseListFilename);
+    const data = node_fs_1.default.readFileSync(courseListFilename).toString();
+    GetCourseList(data);
+}
+else {
+    console.log("No cache found! Downloading...");
+    node_https_1.default.get(courseListUrl, (response) => {
+        console.log("Got response: ", response.statusCode);
+        let data = "";
+        response.on("data", (chunk) => { data += chunk; });
+        response.on("end", () => {
+            console.log("Writing cache: " + courseListFilename);
+            node_fs_1.default.writeFileSync(courseListFilename, data);
+            GetCourseList(data);
+        });
+    }).on("error", (e) => {
+        console.error(e.message);
+    });
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,53 @@
-console.log("Hello world!");
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+import https from "node:https";
+import fs from "node:fs";
+import { JSDOM } from "jsdom";
+
+type Course = {
+    code: string;
+    number: string;
+}
+
+const courseListUrl = "https://www.sis.itu.edu.tr/EN/student/undergraduate/course-plans/plans/BLGE/201810.html";
+const courseListFilename = "CourseList.html";
+
+
+function GetCourseCodes(document: Document): Course[] {
+    const elements: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('a[title="Click For Course Catalogue Form."]');
+    const result: Course[] = [];
+    const regex = /\s+/;
+    elements.forEach((element) => {
+        const splits = element.innerHTML.split(regex);
+        result.push({ code: splits[0], number: splits[1] });
+    });
+    return result;
+}
+
+function GetCourseList(data: string) {
+    const jsdom = new JSDOM(data, { url: courseListUrl });
+    const courseCodes = GetCourseCodes(jsdom.window.document);
+    courseCodes.forEach((course) => {
+        console.log(JSON.stringify(course));
+    });
+}
+
+if (fs.existsSync(courseListFilename)) {
+    console.log("Reading from cache: " + courseListFilename);
+    const data = fs.readFileSync(courseListFilename).toString();
+    GetCourseList(data);
+} else {
+    console.log("No cache found! Downloading...");
+    https.get(courseListUrl, (response) => {
+        console.log("Got response: ", response.statusCode);
+        let data = "";
+        response.on("data", (chunk) => { data += chunk });
+        response.on("end", () => {
+            console.log("Writing cache: " + courseListFilename);
+            fs.writeFileSync(courseListFilename, data);
+            GetCourseList(data);
+        });
+    }).on("error", (e) => {
+        console.error(e.message);
+    })
+}
