94170deeec12278808e6539538d95c902ae47b95
spaceymonk <b.kaan.ozkan@gmail.com>
2024-07-10 08:55:29 +0300

    Final

--- index.js
+++ index.js
@@ -4,46 +4,69 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-const node_https_1 = __importDefault(require("node:https"));
 const node_fs_1 = __importDefault(require("node:fs"));
 const jsdom_1 = require("jsdom");
+const node_url_1 = require("node:url");
 const courseListUrl = "https://www.sis.itu.edu.tr/EN/student/undergraduate/course-plans/plans/BLGE/201810.html";
 const courseListFilename = "CourseList.html";
+const courseDetailsUrl = "https://www.sis.itu.edu.tr/EN/student/undergraduate/course-information/course-information.php";
+const courseDetailsFilename = "CourseDetails.html";
 function GetCourseCodes(document) {
     const elements = document.querySelectorAll('a[title="Click For Course Catalogue Form."]');
     const result = [];
+    const regex = /\s+/;
     elements.forEach((element) => {
-        const text = element.innerHTML;
-        const regex = /\s+/;
-        const splits = text.split(regex);
-        result.push({ code: splits[0], number: splits[1] });
+        const splits = element.innerHTML.split(regex);
+        result.push({ subj: splits[0], numb: splits[1] });
     });
     return result;
 }
-function GetCourseList(data) {
-    const jsdom = new jsdom_1.JSDOM(data, { url: courseListUrl });
-    const courseCodes = GetCourseCodes(jsdom.window.document);
-    courseCodes.forEach((course) => {
-        console.log(JSON.stringify(course));
+function GetCourseDetails(course) {
+    const body = new node_url_1.URLSearchParams(course);
+    return new Promise((resolve, reject) => {
+        fetch(courseDetailsUrl, { method: "POST", body })
+            .then((res) => res.text())
+            .then((text) => resolve(text))
+            .catch(e => reject(e));
     });
 }
-if (node_fs_1.default.existsSync(courseListFilename)) {
-    console.log("Reading from cache: " + courseListFilename);
-    const data = node_fs_1.default.readFileSync(courseListFilename).toString();
-    GetCourseList(data);
+async function GetCourseListHtml(text) {
+    let payload = "";
+    const courseCodes = GetCourseCodes(new jsdom_1.JSDOM(text, { url: courseListUrl }).window.document);
+    for (let index = 0; index < courseCodes.length; index++) {
+        const course = courseCodes[index];
+        console.log(`[${index + 1}/${courseCodes.length}] Processing ${course.subj} ${course.numb}`);
+        console.log("    Downloading...");
+        const courseDetail = await GetCourseDetails(course);
+        console.log("    Extracting...");
+        const jsdom = new jsdom_1.JSDOM(courseDetail, { url: courseDetailsUrl });
+        const detailsElem = jsdom.window.document.querySelector("body > div > div.main-container > div > div:nth-child(3)");
+        if (detailsElem === null) {
+            throw new Error("Details element is not present for: " + JSON.stringify(course));
+        }
+        console.log("    Saving...");
+        payload += detailsElem.innerHTML;
+    }
+    return payload;
 }
-else {
-    console.log("No cache found! Downloading...");
-    node_https_1.default.get(courseListUrl, (response) => {
-        console.log("Got response: ", response.statusCode);
-        let data = "";
-        response.on("data", (chunk) => { data += chunk; });
-        response.on("end", () => {
-            console.log("Writing cache: " + courseListFilename);
-            node_fs_1.default.writeFileSync(courseListFilename, data);
-            GetCourseList(data);
-        });
-    }).on("error", (e) => {
-        console.error(e.message);
-    });
+async function main() {
+    if (node_fs_1.default.existsSync(courseListFilename)) {
+        console.log("Reading from cache: " + courseListFilename);
+        const text = node_fs_1.default.readFileSync(courseListFilename).toString();
+        const html = await GetCourseListHtml(text);
+        node_fs_1.default.writeFileSync(courseDetailsFilename, html);
+    }
+    else {
+        console.log("No cache found! Downloading...");
+        fetch(courseListUrl)
+            .then((res) => res.text())
+            .then(async (text) => {
+            console.log("Writing cache...");
+            node_fs_1.default.writeFileSync(courseListFilename, text);
+            const html = await GetCourseListHtml(text);
+            node_fs_1.default.writeFileSync(courseDetailsFilename, html);
+        })
+            .catch((e) => { console.error(e.message); });
+    }
 }
+main();
--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-import https from "node:https";
 import fs from "node:fs";
 import { JSDOM } from "jsdom";
+import { URLSearchParams } from "node:url";
 
 type Course = {
-    code: string;
-    number: string;
+    subj: string;
+    numb: string;
 }
 
 const courseListUrl = "https://www.sis.itu.edu.tr/EN/student/undergraduate/course-plans/plans/BLGE/201810.html";
 const courseListFilename = "CourseList.html";
-
+const courseDetailsUrl = "https://www.sis.itu.edu.tr/EN/student/undergraduate/course-information/course-information.php";
+const courseDetailsFilename = "CourseDetails.html";
 
 function GetCourseCodes(document: Document): Course[] {
     const elements: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('a[title="Click For Course Catalogue Form."]');
@@ -19,35 +20,60 @@ function GetCourseCodes(document: Document): Course[] {
     const regex = /\s+/;
     elements.forEach((element) => {
         const splits = element.innerHTML.split(regex);
-        result.push({ code: splits[0], number: splits[1] });
+        result.push({ subj: splits[0], numb: splits[1] });
     });
     return result;
 }
 
-function GetCourseList(data: string) {
-    const jsdom = new JSDOM(data, { url: courseListUrl });
-    const courseCodes = GetCourseCodes(jsdom.window.document);
-    courseCodes.forEach((course) => {
-        console.log(JSON.stringify(course));
-    });
-}
-
-if (fs.existsSync(courseListFilename)) {
-    console.log("Reading from cache: " + courseListFilename);
-    const data = fs.readFileSync(courseListFilename).toString();
-    GetCourseList(data);
-} else {
-    console.log("No cache found! Downloading...");
-    https.get(courseListUrl, (response) => {
-        console.log("Got response: ", response.statusCode);
-        let data = "";
-        response.on("data", (chunk) => { data += chunk });
-        response.on("end", () => {
-            console.log("Writing cache: " + courseListFilename);
-            fs.writeFileSync(courseListFilename, data);
-            GetCourseList(data);
-        });
-    }).on("error", (e) => {
-        console.error(e.message);
+function GetCourseDetails(course: Course): Promise<string> {
+    const body = new URLSearchParams(course);
+    return new Promise((resolve, reject) => {
+        fetch(courseDetailsUrl, { method: "POST", body })
+            .then((res) => res.text())
+            .then((text) => resolve(text))
+            .catch(e => reject(e));
     })
 }
+
+async function GetCourseListHtml(text: string): Promise<string> {
+    let payload = "";
+    const courseCodes = GetCourseCodes(new JSDOM(text, { url: courseListUrl }).window.document);
+    for (let index = 0; index < courseCodes.length; index++) {
+        const course = courseCodes[index];
+        console.log(`[${index + 1}/${courseCodes.length}] Processing ${course.subj} ${course.numb}`);
+
+        console.log("    Downloading...");
+        const courseDetail = await GetCourseDetails(course);
+        console.log("    Extracting...");
+        const jsdom = new JSDOM(courseDetail, { url: courseDetailsUrl });
+        const detailsElem = jsdom.window.document.querySelector("body > div > div.main-container > div > div:nth-child(3)");
+        if (detailsElem === null) {
+            throw new Error("Details element is not present for: " + JSON.stringify(course));
+        }
+        console.log("    Saving...");
+        payload += detailsElem.innerHTML;
+    }
+    return payload;
+}
+
+async function main() {
+    if (fs.existsSync(courseListFilename)) {
+        console.log("Reading from cache: " + courseListFilename);
+        const text = fs.readFileSync(courseListFilename).toString();
+        const html = await GetCourseListHtml(text);
+        fs.writeFileSync(courseDetailsFilename, html);
+    } else {
+        console.log("No cache found! Downloading...");
+        fetch(courseListUrl)
+            .then((res) => res.text())
+            .then(async (text) => {
+                console.log("Writing cache...");
+                fs.writeFileSync(courseListFilename, text);
+                const html = await GetCourseListHtml(text);
+                fs.writeFileSync(courseDetailsFilename, html);
+            })
+            .catch((e) => { console.error(e.message); });
+    }
+}
+
+main();
